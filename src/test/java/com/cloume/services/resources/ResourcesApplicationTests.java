c863791d4fcb6564f9c32e2ff792305f3f574244
gxu <gxu@cloume.com>
2016-07-06 13:07:40 +0800

    init

---
+++ src/test/java/com/cloume/services/resources/ResourcesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cloume.services.resources;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ResourcesApplication.class)
+@WebAppConfiguration
+public class ResourcesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
